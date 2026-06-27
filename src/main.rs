763f427b2bc9a15290c610162d722a805e010dd8
bjackson312006 <blakejackson312006@gmail.com>
2026-06-26 18:24:56 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use embassy_mcxa::flexcan;
 async fn main(_spawner: Spawner) {
     let p = embassy_mcxa::init(Config::default());
 
-    use embassy_mcxa::flexcan::filter::{Filter, filters, StandardId, ExtendedId};
+    use embassy_mcxa::flexcan::filter::{Filter, FilterConfig, filters, StandardId, ExtendedId};
     use embassy_mcxa::flexcan::classic::{FlexCan, FlexCanConfig};
     use embassy_mcxa::peripherals::{CAN0, CAN1};
  
@@ -19,7 +19,7 @@ async fn main(_spawner: Spawner) {
     const EXAMPLE_MESSAGE_THREE: StandardId = StandardId::new(0x100).expect("Invalid ID (too large).");
     const EXAMPLE_MESSAGE_THREE_MASK: StandardId = StandardId::new(0x7F0).expect("Invalid mask (too large).");
 
-    let filters = filters!(
+    let filters: FilterConfig = filters!(
         Filter::Standard(EXAMPLE_MESSAGE_ONE),
         Filter::Extended(EXAMPLE_MESSAGE_TWO),
         Filter::StandardMasked { id: EXAMPLE_MESSAGE_THREE, mask: EXAMPLE_MESSAGE_THREE_MASK },
@@ -27,7 +27,9 @@ async fn main(_spawner: Spawner) {
 
     let can = FlexCan::new(p.CAN0, p.P1_3, p.P1_2, FlexCanConfig {
         protocol_exception: true,
-        filters
+        filters: filters!(
+            Filter::Standard(EXAMPLE_MESSAGE_ONE),
+        )
     }).expect("Failed to init FlexCan!!");
 
     loop {}
